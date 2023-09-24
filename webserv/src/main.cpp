22e6cf41dbe1ed9187cf99b5e7012fbced312065
Anahit K <anikakasamanyan@gmail.com>
2023-09-24 22:05:13 +0200

    freaking slash

--- webserv/src/main.cpp
+++ webserv/src/main.cpp
@@ -6,30 +6,30 @@ int main(int argc, char **argv)
 
 	PRINT << RESET_LINE << PURPLE "\t\t... STARTING THE SERVER ..." << RESET_LINE;
 
-	// conf->config.host = "0.0.0.0";
-	// conf->config.port = "2000";
-    // conf->config.locations["/"].methodDelete = true;
-    // conf->config.locations["/"].methodGet = true;
-    // conf->config.locations["/"].methodPost = true;
-    // conf->config.maxBody = "80000";
-    // conf->config.root = "/html";
-    // conf->config.locations["/uploads/"].methodPost = true;
-    // conf->config.locations["/uploads/"].methodGet = true;
-    // conf->config.locations["/uploads/"].methodDelete = true;
-    // conf->config.locations["/uploads/"].dirListing = true;
+	conf->config.host = "0.0.0.0";
+	conf->config.port = "2000";
+    conf->config.locations["/"].methodDelete = true;
+    conf->config.locations["/"].methodGet = true;
+    conf->config.locations["/"].methodPost = true;
+    conf->config.maxBody = "80000";
+    conf->config.root = "/html/";
+    conf->config.locations["/uploads/"].methodPost = true;
+    conf->config.locations["/uploads/"].methodGet = true;
+    conf->config.locations["/uploads/"].methodDelete = true;
+    conf->config.locations["/uploads/"].dirListing = true;
 
-    // conf->config.locations["/errorHtml/"].methodPost = false;
-    // conf->config.locations["/errorHtml/"].methodGet = true;
-    // conf->config.locations["/errorHtml/"].methodDelete = false;
-    // conf->config.locations["/errorHtml/"].dirListing = true;
+    conf->config.locations["/errorHtml/"].methodPost = false;
+    conf->config.locations["/errorHtml/"].methodGet = true;
+    conf->config.locations["/errorHtml/"].methodDelete = false;
+    conf->config.locations["/errorHtml/"].dirListing = true;
 	
-    // conf->config.locations["/folder/"].methodPost = false;
-    // conf->config.locations["/folder/"].methodGet = true;
-    // conf->config.locations["/folder/"].methodDelete = false;
-    // // conf->config.locations["/folder/"].dirListing = true;
-	// conf->config.locations["/folder/"].redirection = "/";
-	// conf->config.locations["/folder/"].defaultFile = "/errorHtml/500.html";
-    // conf->config.CGIDir = "";
+    conf->config.locations["/folder/"].methodPost = false;
+    conf->config.locations["/folder/"].methodGet = true;
+    conf->config.locations["/folder/"].methodDelete = false;
+    // conf->config.locations["/folder/"].dirListing = true;
+	conf->config.locations["/folder/"].redirection = "/";
+	conf->config.locations["/folder/"].defaultFile = "/errorHtml/500.html";
+    conf->config.CGIDir = "";
 	/* vector parsing structs */
 	Server	server(conf->config);
 
